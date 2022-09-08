2ef014dd801828d22d327a0d15414f39c1ae91e5
Tishchenko_D <tishchenko.1993@list.ru>
2022-09-08 10:53:38 +0300

    Задача №2 Набор произведений от 1 до N

---
+++ task02_02.py
@@ -0,0 +1,23 @@
+# 2 - Напишите программу, которая принимает на вход число N и выдает
+# набор произведений (набор - это список) чисел от 1 до N.
+# Не используйте функцию math.factorial.
+
+# Пример:
+# - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
+
+def list_element():
+    try:
+        number = int(input('Введите число: '))
+        if number < 0:
+            return print('Последовательности для отрицательных чисел не существует')
+        list_number = []
+        element = 1
+        for i in range(1, number + 1):
+            element *= i
+            list_number.append(element)
+        return print(list_number)
+    except (ValueError):
+        return list_element()
+
+
+list_element()
